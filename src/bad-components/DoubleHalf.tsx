cca32eb5e452776a1423ba26cfae9abbdfbde00b
TrevorBrennan11 <trevor.brennan@gmail.com>
2024-02-29 19:59:31 -0500

    Added DoubleHalf component, removed unnecessary file "DoubleHalfState.tsx"

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+interface setValueProps {
+    //Type is a function that consumes a nothing and returns nothing
+    setValue: () => void;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Doubler({ setValue }: setValueProps): JSX.Element {
+    return <Button onClick={setValue}>Double</Button>;
+}
+
+function Halver({ setValue }: setValueProps): JSX.Element {
+    return <Button onClick={setValue}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [value, setValue] = useState<number>(10);
+    const doubleValue = () => setValue(value * 2);
+    const halfValue = () => setValue(value * 0.5);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
-                The current value is: <span>{dhValue}</span>
+                The current value is: <span>{value}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler setValue={doubleValue}></Doubler>
+            <Halver setValue={halfValue}></Halver>
         </div>
     );
 }
