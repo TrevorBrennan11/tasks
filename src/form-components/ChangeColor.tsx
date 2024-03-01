26e8675f1e03d8033a4fc5a78bdb46911bc7e077
TrevorBrennan11 <trevor.brennan@gmail.com>
2024-03-01 10:52:53 -0500

    Solved the rest of task 10

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const COLORS = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "blueviolet",
+        "darkblue",
+        "sienna"
+    ];
+
+    const [color, setColor] = useState<string>();
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Group controlId="formChangeColor">
+                {COLORS.map((colour: string) => (
+                    <Form.Check
+                        key={colour}
+                        inline
+                        type="radio"
+                        name="colorPicker"
+                        onChange={(c) => setColor(c.target.value)}
+                        id={colour}
+                        label={colour}
+                        style={{ backgroundColor: colour }}
+                        value={colour}
+                        checked={color === colour}
+                    />
+                ))}
+            </Form.Group>
+            <div style={{ backgroundColor: color }} data-testId="colored-box">
+                {color}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,22 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="formOptions">
+                <Form.Select
+                    value={answer}
+                    onChange={(o) => setAnswer(o.target.value)}
+                >
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <label>{answer === expectedAnswer ? "✔️" : "❌"}</label>
         </div>
     );
 }
